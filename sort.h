cd7beb06cbf25c8fba84deb054882de1c7c23202
Femi Mehalayese <megafemgroup@gmail.com>
2024-04-26 14:27:11 +0100

    bubble

--- sort.h
+++ sort.h
@@ -15,7 +15,6 @@ typedef struct listint_s
     struct listint_s *next;
 } listint_t;
 
-#include <stddef.h>
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
 void bubble_sort(int *array, size_t size);
