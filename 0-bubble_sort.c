f57bd80ff3f36bddb61554c4a3e0eb28c8ef83a4
Femi Mehalayese <megafemgroup@gmail.com>
2024-04-26 14:24:41 +0100

    bubble

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -15,10 +15,10 @@ void bubble_sort(int *array, size_t size)
 
 	if (array == NULL || size < 2)
 		return;
-	for (i = 0; i < size -1; i++)
+	for (i = 0; i < size - 1; i++)
 	{
 		swapped = 0;
-		for(j = 0; j < size - i -1; j++)
+		for (j = 0; j < size - i - 1; j++)
 		{
 			if (array[j] > array[j + 1])
 			{
