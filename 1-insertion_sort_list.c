afd847da44abe6bc8c05a21af4a6de9ef56ab7fa
Femi Mehalayese <megafemgroup@gmail.com>
2024-04-29 11:43:27 +0100

    selection sort

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -9,42 +9,42 @@
 
 void insertion_sort_list(listint_t **list)
 {
-	listint_t *current;
-	listint_t *prev_node;
-	listint_t *next_node;
-	
-	if (list == NULL || *list == NULL || (*list)->next == NULL)
-		return;
-	current = (*list)->next;
-	while (current != NULL)
-	{
-		prev_node = current->prev;
-		next_node = current->next;
-		while (prev_node != NULL && prev_node->n > current->n)
-		{
-			prev_node = prev_node->prev;
-		}
-		if (prev_node != current->prev)
-		{
-			current->prev->next = next_node;
-			if (next_node != NULL)
-				next_node->prev = current->prev;
-			if (prev_node == NULL)
-			{
-				current->next = *list;
-				current->prev = NULL;
-				(*list)->prev = current;
-				*list = current;
-			}
-			else
-			{
-				prev_node->next->prev = current;
-				current->next = prev_node->next;
-				prev_node->next = current;
-				current->prev = prev_node;
-			}
-			print_list(*list);
-		}
-		current = next_node;
-	}
+        listint_t *current;
+        listint_t *prev_node;
+        listint_t *next_node;
+
+        if (list == NULL || *list == NULL || (*list)->next == NULL)
+                return;
+        current = (*list)->next;
+        while (current != NULL)
+        {
+                prev_node = current->prev;
+                next_node = current->next;
+                while (prev_node != NULL && prev_node->n > current->n)
+                {
+                        prev_node = prev_node->prev;
+                }
+                if (prev_node != current->prev)
+                {
+                        current->prev->next = next_node;
+                        if (next_node != NULL)
+                                next_node->prev = current->prev;
+                        if (prev_node == NULL)
+                        {
+                                current->next = *list;
+                                current->prev = NULL;
+                                (*list)->prev = current;
+                                *list = current;
+                        }
+                        else
+                        {
+                                prev_node->next->prev = current;
+                                current->next = prev_node->next;
+                                prev_node->next = current;
+                                current->prev = prev_node;
+                        }
+                        print_list(*list);
+                }
+                current = next_node;
+        }
 }
